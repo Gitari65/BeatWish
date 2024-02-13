024d2cb66c624cdd1744d0b6ac447ae13f984e93
Gitari65 <agitari65@gmail.com>
2024-02-13 21:25:20 +0300

    adding navigation pages

--- src/navigator/Navigation.js
+++ src/navigator/Navigation.js
@@ -1,3 +1,24 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from '../screens/Home'
+import Welcome from '../screens/Welcome';
+
+const Stack = createStackNavigator();
+const screenOptionStyle = {
+    headerStyle: {
+        backgroundColor: "#9AC4F8",
+    },
+    headerTintColor: "white",
+    headerBackTitle: "Back",
+    };
+
+    const HomeStack = () => {
+    return (
+        <Stack.Navigator screenOptions={screenOptionStyle}>
+            {/* <Stack.Screen name="Home" component={Home} /> */}
+            <Stack.Screen name="Welcome" component={Welcome}/>
+        </Stack.Navigator>
+    );
+}
+
+export default HomeStack;
