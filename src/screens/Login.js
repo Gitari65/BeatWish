5b443e4fda458685b5a81a02470bfe67d019265a
Gitari65 <agitari65@gmail.com>
2024-02-14 13:42:36 +0300

    login page

---
+++ src/screens/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+
+import React from 'react'
+
+export default function Login() {
+    
+  return (
+   <View style={styles.container} >
+   
+
+   </View>
+  )
+  const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        
+        alignItems: "center",
+        backgroundColor:"#000940"
+    }});
+  
+}
