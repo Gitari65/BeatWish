00d2c17f7c1adbda7e88f0e35ae7ae12773ae9c3
Gitari65 <agitari65@gmail.com>
2024-02-15 15:34:14 +0300

    styling my screens

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -2,16 +2,20 @@ import React from "react";
 import { View, Text, StyleSheet ,Image} from "react-native";
 import { useFonts,Montserrat_200ExtraLight,Montserrat_400Regular } from "@expo-google-fonts/montserrat"; 
 import logoImage from '../assets/img/logo2.png';
+import { Merienda_300Light,Merienda_400Regular } from "@expo-google-fonts/merienda";
+
 
 const Welcome = () => {
     let [fontsLoaded] = useFonts({
-        Montserrat_200ExtraLight,
-        Montserrat_400Regular,
+      Merienda:
+        Merienda_400Regular
       });
     return (
         <View style={styles.container}>
-        <Text style={styles.text}>Welcome Screen</Text>
+        
         <Image source={logoImage} style={styles.imageStyle} alt="logo"/>
+        <Text style={styles.text}>Beat Wish</Text>
+        <Text style={styles.text2}>Request the Beat, Set the Heat!</Text>
         </View>
     );
     }
@@ -21,14 +25,30 @@ const Welcome = () => {
             flex: 1,
             
             alignItems: "center",
+            backgroundColor:"#000940"
         },
         text:{
-            fontFamily: "Montserrat_400Regular",
+            fontFamily: 'Merienda',
+            color:"white",
+            transform:"translateY(30px)",
+            fontSize:30,
+            width:150
         },
         imageStyle:{
-            width: 200,
-            height: 200,
+            width: 220,
+            height: 220,
+        },
+        text2:{
+            color:"#CFA118",
+            fontSize:20,
+            transform:"translateY(30px)",
+            fontFamily: 'Merienda',
+        },
+        text3:{
+            color:"white",
+            fontSize:22
         }
+
     });
 
 export default Welcome;
