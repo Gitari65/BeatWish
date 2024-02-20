7f44c94f612b1661c7b927cbecf9b8714a759fa7
Gitari65 <agitari65@gmail.com>
2024-02-20 12:55:07 +0300

    bottom tabs implementation

--- src/navigator/Navigation.js
+++ src/navigator/Navigation.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from '../screens/Home'
+
 import Welcome from '../screens/Welcome';
 import Login from "../screens/Login";
 import Session from "../screens/Session";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { BottomTabBar,createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeTab from "../screens/BottomTabs/HomeTab";
+import ProfileTab from "../screens/BottomTabs/ProfileTab";
+import SessionTab from "../screens/BottomTabs/SessionTab";
 
 const Stack = createStackNavigator();
+const Tab=createBottomTabNavigator();
 const screenOptionStyle = {
     headerStyle: {
         backgroundColor: "#000940",
@@ -14,15 +21,54 @@ const screenOptionStyle = {
     headerBackTitle: "Back",
     };
 
-    const HomeStack = () => {
-    return (
-        <Stack.Navigator screenOptions={screenOptionStyle}>
-            {/* <Stack.Screen name="Home" component={Home} /> */}
-        {/* <Stack.Screen name="Welcome" component={Welcome}/> */}
-            {/* <Stack.Screen name="Login" component={Login}/> */}
-        <Stack.Screen name="Session" component={Session}/>
-        </Stack.Navigator>
-    );
-}
+    const AppNavigation = () => {
+       
+            return (
+                <Stack.Navigator screenOptions={screenOptionStyle}>
+                   
+                    <Stack.Screen name="Login" component={Login}/>
+                    <Stack.Screen name="Session" component={Session}/>
+                    <Stack.Screen name="Home" component={HomeStack}/>
+                </Stack.Navigator>
+            )
+        };
+      
 
-export default HomeStack;
+    
+    const HomeStack=()=>{
+        return(
+            
+                <Tab.Navigator>
+                 <Tab.Screen name="HomeTab" component={HomeTab}
+                 options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="home" color={color} size={size} />
+                    ),
+                  }
+                 }
+                  />
+                <Tab.Screen name="ProfileTab" component={ProfileTab}
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="account" color={color} size={size} />
+                    ),
+                  }
+                }
+                />
+                <Tab.Screen name="SessionTab" component={SessionTab}
+                options={{
+                    tabBarLabel: 'Session',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="music" color={color} size={size} />
+                    ),
+                  }
+                }
+                />
+    
+                </Tab.Navigator>
+            )
+    }
+
+export default AppNavigation;
