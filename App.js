5a961bf9cd175b3a9c6260c90524288f6a9a6033
Gitari65 <agitari65@gmail.com>
2024-02-24 13:49:05 +0300

    home tab components: live sessions,accont,user sessions

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
  import React  from "react";
  import { NavigationContainer } from "@react-navigation/native";
- import HomeStack from "./src/navigator/Navigation";
+ import AppNavigation from "./src/navigator/Navigation";
+ import { useEffect ,useState} from "react";
+ import Welcome from "./src/screens/Welcome";
 
 
   export default function App() {
+    const [isLoading,setIsLoading]=useState(true);
+    useEffect(()=>{
+        const timer=setTimeout(()=>{
+            setIsLoading(false);
+        },3000);
+
+        return ()=>clearTimeout(timer);
+
+    },[])
     return (
       <NavigationContainer>
-        <HomeStack />
+      {
+      isLoading?(<Welcome/>) : (<AppNavigation />)
+      }
+       
        
       </NavigationContainer>
     );
