300f4f305ea8fd34aa9b4332eabd77a5db1fb504
Gitari65 <agitari65@gmail.com>
2024-03-12 12:48:04 +0300

    adding a sidemenu drawer

---
+++ src/constants/DrawerItems.js
@@ -0,0 +1,25 @@
+export default[{
+    name: 'Home',
+    iconType: 'Material',
+    iconName: 'home'
+
+
+},
+{
+    name:'Profile',
+    iconType:'Material',
+    iconName:'face-profile'
+},
+{
+    name:'Settings',
+    iconType:'Feather',
+    iconName:'settings'
+},
+{
+    name:'Saved Sessions',
+    iconType:'Material',
+    iconName:'bookmark-check-outline'
+},
+
+
+]
