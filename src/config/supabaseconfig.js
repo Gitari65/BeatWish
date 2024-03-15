2ce4f306e8ebfa57f685b0baae17720dcdadd60d
Gitari65 <agitari65@gmail.com>
2024-03-15 13:03:08 +0300

    setting up supabase client

--- src/config/supabaseconfig.js
+++ src/config/supabaseconfig.js
@@ -1,6 +1,6 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = 'https://uncidnwsltcpprioqwwn.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuY2lkbndzbHRjcHByaW9xd3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2OTExNzksImV4cCI6MjAyMjI2NzE3OX0.EtrtSHAJgmiT0u3VtBvUJYixPoFwCG-ZuVOI3izAdn8';
+const supabaseUrl = `https://uncidnwsltcpprioqwwn.supabase.co`;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuY2lkbndzbHRjcHByaW9xd3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2OTExNzksImV4cCI6MjAyMjI2NzE3OX0.EtrtSHAJgmiT0u3VtBvUJYixPoFwCG-ZuVOI3izAdn8`;
 
-// export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
